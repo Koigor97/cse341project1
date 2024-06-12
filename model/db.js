76ef7bb8adb223ec59677faf84c7a8fb6db905d8
Koigor97 <turaysamuel101@yahoo.com>
2024-06-12 13:46:52 +0200

    create the bd file for connecting to the database

---
+++ model/db.js
@@ -0,0 +1,35 @@
+// db.js
+const dotenv = require("dotenv");
+const { MongoClient } = require("mongodb");
+const contactsSchema = require("./contactsSchema");
+dotenv.config();
+
+// connecting to mongodb
+const client = new MongoClient(
+  process.env.MONGO_DB_URI.replace("<PASSWORD>", process.env.MONGO_DB_PASSWORD)
+);
+
+let Contacts;
+
+// connecting to mongodb
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    Contacts = client
+      .db("cse341project1")
+      .collection("contacts", contactsSchema);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getting contacts collection
+function getContactsCollection() {
+  if (!Contacts) {
+    throw new Error("Contacts collection not initialized");
+  }
+  return Contacts;
+}
+
+module.exports = { connectDB, getContactsCollection };
