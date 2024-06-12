28eb82cd047921d73c64e93e1a2b1ef31e71b1a5
Koigor97 <turaysamuel101@yahoo.com>
2024-06-12 13:46:16 +0200

    create contactsSchema for the contacts collection

---
+++ model/contactsSchema.js
@@ -0,0 +1,8 @@
+// creating a schema for the contacts collection
+exports.contactSchema = {
+  firstName: "string",
+  lastName: "string",
+  email: "string",
+  favoriteColor: "string",
+  birthday: "string",
+};
