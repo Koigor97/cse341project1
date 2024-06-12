526ba3ee0fa9d3441de74f293cbdc2fdf8792f6a
Koigor97 <turaysamuel101@yahoo.com>
2024-06-12 13:44:11 +0200

    create the app.js file

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+const contactsRoute = require("./routes/contactsRoute");
+
+// instantiating express framework
+const app = express();
+
+// using morgan middleware for logging requests data
+// if the node environment is not production
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+// using json middleware from express
+// to parse json data from the request
+app.use(express.json());
+
+// using the contacts route
+app.use("/api/contacts", contactsRoute);
+
+module.exports = app;
